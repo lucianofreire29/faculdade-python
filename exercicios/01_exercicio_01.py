ba4c936af51e7aa06f0c61dee0207019762e0e54
lucianofreire <lucianofreiredigital29@gmail.com>
2026-09-04 10:33:15 -0300

    feat:01_exercicio

---
+++ exercicios/01_exercicio_01.py
@@ -0,0 +1,15 @@
+"""“Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou
+V-Vespertino ou N- Noturno. Imprima a mensagem “Bom Dia!”, “Boa Tarde!” ou “Boa Noite!” ou “Valor Inválido!”,
+conforme o caso.
+"""
+
+
+turno = str(input("Digite o turno que você Estuda(M-matutino,V-vespetino,N-noturno):"))
+if turno =="M" or turno =="m":
+    print("Bom Dia!")
+elif turno =="v" or turno =="V":
+    print("Boa Tarde!")
+elif turno =="n" or turno =="N":
+    print("Boa Noite!")
+else:
+    print("valor inválido!")
