6db02df6bb87191fc937af7fb974b5a6621bfbd4
lucianofreire <lucianofreiredigital29@gmail.com>
2026-09-15 10:00:22 -0300

    feat: exercicio_09

---
+++ exercicios/09_exercicio.py
@@ -0,0 +1,16 @@
+estoque =["monitor", "teclado", "mouse","headset"]
+
+estoque.append("webcam")
+
+estoque[1] = "teclado mecanico"
+
+if "impressora" in estoque:
+    tem = bool(estoque)
+else:
+    tem = bool(estoque)
+
+estoque.remove("mouse")
+
+print(tem)
+
+print(estoque)
