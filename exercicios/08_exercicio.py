776ca7e2cfb5d0dfabb419490a60174a31c15d20
lucianofreire <lucianofreiredigital29@gmail.com>
2026-09-15 10:22:54 -0300

    feat:exercicio_12

--- exercicios/08_exercicio.py
+++ exercicios/08_exercicio.py
@@ -4,9 +4,8 @@ tvendas = sum(vendas)
 
 medvendas = tvendas / len(vendas)
 
-maiorvendas = max(vendas)
-menorvendas = min(vendas)
 
 print(f"total de vdnas na semana: {tvendas}")
 print(f"media de vendas: {medvendas}")
-print(f"valor maior de vendas: {maiorvendas}, valor menor de vendas:{menorvendas}")
+print(f"valor maior de vendas: {max(vendas)}, valor menor de vendas:{min(vendas)}")
+
---
+++ exercicios/11_exercicio.py
@@ -0,0 +1,9 @@
+rota = ["sao paulo", "campinas", "jundai","sorocaba"]
+
+nova_cidades = ["itu","valinhos"]
+
+rota.append(nova_cidades)
+
+
+print(rota)
+print(f"sorocaba e a {rota.index("sorocaba")} cidade da rota")
